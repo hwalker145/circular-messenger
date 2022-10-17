63c93d2c3f524e5d94c29b457ba8548998fe92a8
hwalker145 <hwalker145@gmail.com>
2022-10-17 08:58:31 -0400

    documentation and tweaking

--- circular.py
+++ circular.py
@@ -1,26 +1,43 @@
+# socket
 import socket as sk
 
-# goes to 'PERSON'
-
+# is the process the 
+# original process (put True) or a 
+# forwarding process (put False)
 IS_ORIGIN = False
+
+# destination info
 DEST_ADDRESS = '10.220.49.117'
-RECV_ADDRESS = '10.220.49.117'
 DEST_SOCKET = 1236
+
+# source info
+RECV_ADDRESS = '10.220.49.117'
 RECV_SOCKET = 1235
 
+# initializing the client socket
 send_socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+send_socket.connect((DEST_ADDRESS, DEST_SOCKET))
 
 if not IS_ORIGIN:
+    #initializing the server socket
     receive_socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
     receive_socket.bind((RECV_ADDRESS, RECV_SOCKET))
 
+    # waiting and accepting the connection
     receive_socket.listen(1)
     cSocket, addr = receive_socket.accept()
 
+    # recv call to grab the message
     message = cSocket.recv(60)
 
-    send_socket.connect((DEST_ADDRESS, DEST_SOCKET))
+    # close socket once message is received
+    receive_socket.close()
+
+    # forwarding the message
     send_socket.sendall(message)
-else: 
-    send_socket.connect((DEST_ADDRESS, DEST_SOCKET))
-    send_socket.sendall(str('Hello Salem!').encode())
+else:
+    # sending the message 'Hello Salem'
+    send_socket.sendall(b'Hello Salem!')
+
+    # close socket once message is sent
+    send_socket.close()
--- fakesalem.py
+++ fakesalem.py
@@ -1,13 +1,22 @@
+# socket
 import socket as sk
 
-receiver_socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-receiver_socket.bind(('10.220.49.117', 1236))
+# source info
+RECV_ADDRESS = '10.220.49.117'
+RECV_SOCKET = 1236
 
-receiver_socket.listen(10)
+# initializing the server socket
+receiver_socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+receiver_socket.bind((RECV_ADDRESS, RECV_SOCKET))
+
+# accept tcp connection
+receiver_socket.listen(1)
 cSocket, addr = receiver_socket.accept()
 
+# grab message
 message = cSocket.recv(60)
 
 print(message.decode())
 
+# close socket once message is received
 receiver_socket.close()
